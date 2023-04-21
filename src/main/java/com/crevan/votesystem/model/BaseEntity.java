0e41684a4f3c7c6ac61bee931c14d5092931ff19
Crevan <a.siverskii@gmail.com>
2023-04-21 21:19:18 +0300

    [add] base class for entities

---
+++ src/main/java/com/crevan/votesystem/model/BaseEntity.java
@@ -0,0 +1,49 @@
+package com.crevan.votesystem.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.domain.Persistable;
+import org.springframework.data.util.ProxyUtils;
+import org.springframework.util.Assert;
+
+@Getter
+@Setter
+@ToString
+@MappedSuperclass
+@Access(value = AccessType.FIELD)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public class BaseEntity implements Persistable<Integer> {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Override
+    public Integer getId() {
+        Assert.notNull(id, "Entity must have id");
+        return id;
+    }
+
+    @Override
+    public boolean isNew() {
+        return id == null;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || !getClass().equals(ProxyUtils.getUserClass(o))) {
+            return false;
+        }
+        BaseEntity that = (BaseEntity) o;
+        return id != null && id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id == null ? 0 : id;
+    }
+}
