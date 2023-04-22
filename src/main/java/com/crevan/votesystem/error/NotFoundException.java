0aca1346add43daa91235024ba67c7b925d2e174
Crevan <a.siverskii@gmail.com>
2023-04-22 15:17:37 +0300

    [add] exceptions

---
+++ src/main/java/com/crevan/votesystem/error/NotFoundException.java
@@ -0,0 +1,7 @@
+package com.crevan.votesystem.error;
+
+public class NotFoundException extends AppException {
+    public NotFoundException(final String message) {
+        super(message);
+    }
+}
