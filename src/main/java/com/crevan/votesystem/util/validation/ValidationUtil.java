b271e7ad37128e956e8272124cafcee87a0bb3db
Crevan <a.siverskii@gmail.com>
2023-04-23 18:42:43 +0300

    [add] ValidationUtil

---
+++ src/main/java/com/crevan/votesystem/util/validation/ValidationUtil.java
@@ -0,0 +1,23 @@
+package com.crevan.votesystem.util.validation;
+
+import com.crevan.votesystem.HasId;
+import com.crevan.votesystem.error.IllegalRequestDataException;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class ValidationUtil {
+    public static void checkNew(final HasId bean) {
+        if (!bean.isNew()) {
+            throw new IllegalRequestDataException(bean.getClass().getSimpleName() + " must be new (id=null)");
+        }
+    }
+
+    //  Conservative when you reply, but accept liberally (http://stackoverflow.com/a/32728226/548473)
+    public static void assureIdConsistent(final HasId bean, final int id) {
+        if (bean.isNew()) {
+            bean.setId(id);
+        } else if (bean.id() != id) {
+            throw new IllegalRequestDataException(bean.getClass().getSimpleName() + " must has id=" + id);
+        }
+    }
+}
