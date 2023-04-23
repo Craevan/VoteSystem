f1c5ef0a4bf67dc7cee58e855d655deeacb62d9f
Crevan <a.siverskii@gmail.com>
2023-04-23 18:42:31 +0300

    [add] IllegalRequestDataException

---
+++ src/main/java/com/crevan/votesystem/error/IllegalRequestDataException.java
@@ -0,0 +1,8 @@
+package com.crevan.votesystem.error;
+
+public class IllegalRequestDataException extends AppException {
+
+    public IllegalRequestDataException(final String message) {
+        super(message);
+    }
+}
