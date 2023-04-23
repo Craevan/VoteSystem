8c0e58eebc49e71caad04a0a2ac4fd1103ae0d66
Crevan <a.siverskii@gmail.com>
2023-04-23 17:19:17 +0300

    [update] add @JsonIgnore for password, hasRole()

--- src/main/java/com/crevan/votesystem/model/User.java
+++ src/main/java/com/crevan/votesystem/model/User.java
@@ -1,5 +1,7 @@
 package com.crevan.votesystem.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.*;
 import lombok.*;
@@ -23,15 +25,17 @@ public class User extends NamedEntity implements Serializable {
     private String email;
 
     @NotBlank
+    @JsonIgnore
     @Size(max = 256)
     @Column(name = "password", nullable = false)
     private String password;
 
-    @Column(name = "is_active", nullable = false)
+    @Column(name = "is_active", nullable = false, columnDefinition = "bool default true")
     private boolean isActive = true;
 
-    @Column(name = "registered", nullable = false, updatable = false)
     @NotNull
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    @Column(name = "registered", nullable = false, columnDefinition = "timestamp default now()", updatable = false)
     private Date registered = new Date();
 
     @Column(name = "role")
@@ -40,4 +44,8 @@ public class User extends NamedEntity implements Serializable {
             uniqueConstraints = @UniqueConstraint(columnNames = {"user_id", "role"}, name = "users_roles_unique"))
     @ElementCollection(fetch = FetchType.EAGER)
     private Set<Role> roles;
+
+    public boolean hasRole(Role role) {
+        return roles != null && roles.contains(role);
+    }
 }
