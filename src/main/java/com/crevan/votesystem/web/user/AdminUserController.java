ea5d3b4ebdf70cb2c2803c9d095b25f19d49dfa0
Crevan <a.siverskii@gmail.com>
2023-04-23 18:45:17 +0300

    [update] add methods createWithLocation(); update(); enable()

--- src/main/java/com/crevan/votesystem/web/user/AdminUserController.java
+++ src/main/java/com/crevan/votesystem/web/user/AdminUserController.java
@@ -1,12 +1,17 @@
 package com.crevan.votesystem.web.user;
 
-import com.crevan.votesystem.error.NotFoundException;
 import com.crevan.votesystem.model.User;
+import com.crevan.votesystem.util.validation.ValidationUtil;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @Slf4j
@@ -15,6 +20,25 @@ import java.util.List;
 public class AdminUserController extends AbstractUserController {
 
     static final String REST_URL = "/api/admin/users";
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<User> createWithLocation(@Valid @RequestBody final User user) {
+        log.info("create {}", user);
+        ValidationUtil.checkNew(user);
+        User created = repository.prepareAndSave(user);
+        URI uriOfNewResource = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path(REST_URL + "/{id}")
+                .buildAndExpand(created.getId()).toUri();
+        return ResponseEntity.created(uriOfNewResource).body(created);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void update(@Valid @RequestBody final User user, @PathVariable final int id) {
+        log.info("updating {} with id={}", user, id);
+        ValidationUtil.assureIdConsistent(user, id);
+        repository.prepareAndSave(user);
+    }
 
     @GetMapping("/")
     public List<User> getAll() {
@@ -25,8 +49,7 @@ public class AdminUserController extends AbstractUserController {
     @GetMapping("/by-email")
     public User getByEmail(@RequestParam final String email) {
         log.info("get user with email={}", email);
-        return repository.findByEmailIgnoreCase(email)
-                .orElseThrow(() -> new NotFoundException("User with email=" + email.toLowerCase() + " not found"));
+        return repository.getExistedByEmail(email);
     }
 
     @Override
@@ -40,5 +63,14 @@ public class AdminUserController extends AbstractUserController {
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable final int id) {
         super.delete(id);
+    }
+
+    @Transactional
+    @PatchMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void enable(@PathVariable final int id, @RequestParam final boolean enabled) {
+        log.info(enabled ? "enable {}" : "disable {}", id);
+        User user = repository.getExisted(id);
+        user.setActive(enabled);
     }
 }
