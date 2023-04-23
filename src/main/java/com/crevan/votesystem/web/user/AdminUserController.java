d6189f1c4de34036e6eb22eae4f03de93f8dad67
Crevan <a.siverskii@gmail.com>
2023-04-23 17:22:01 +0300

    [update] add getAll and getByEmail methods; add logging

--- src/main/java/com/crevan/votesystem/web/user/AdminUserController.java
+++ src/main/java/com/crevan/votesystem/web/user/AdminUserController.java
@@ -1,15 +1,33 @@
 package com.crevan.votesystem.web.user;
 
+import com.crevan.votesystem.error.NotFoundException;
 import com.crevan.votesystem.model.User;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@Slf4j
 @RestController
 @RequestMapping(value = AdminUserController.REST_URL, produces = MediaType.APPLICATION_JSON_VALUE)
 public class AdminUserController extends AbstractUserController {
 
     static final String REST_URL = "/api/admin/users";
+
+    @GetMapping("/")
+    public List<User> getAll() {
+        log.info("getting all users");
+        return repository.findAll();
+    }
+
+    @GetMapping("/by-email")
+    public User getByEmail(@RequestParam final String email) {
+        log.info("get user with email={}", email);
+        return repository.findByEmailIgnoreCase(email)
+                .orElseThrow(() -> new NotFoundException("User with email=" + email.toLowerCase() + " not found"));
+    }
 
     @Override
     @GetMapping("/{id}")
