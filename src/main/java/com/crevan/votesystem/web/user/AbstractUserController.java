90949b686a5ff24782cdff8fbe328e896d5c1f7b
Crevan <a.siverskii@gmail.com>
2023-04-23 17:21:01 +0300

    [update] add UniqueMailValidator

--- src/main/java/com/crevan/votesystem/web/user/AbstractUserController.java
+++ src/main/java/com/crevan/votesystem/web/user/AbstractUserController.java
@@ -4,18 +4,28 @@ import com.crevan.votesystem.model.User;
 import com.crevan.votesystem.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 
 @Slf4j
 public abstract class AbstractUserController {
     @Autowired
     protected UserRepository repository;
 
-    public User get(int id) {
+    @Autowired
+    private UniqueMailValidator emailValidator;
+
+    @InitBinder
+    protected void initBinder(final WebDataBinder binder) {
+        binder.addValidators(emailValidator);
+    }
+
+    public User get(final int id) {
         log.info("get user with id={}", id);
         return repository.getExisted(id);
     }
 
-    public void delete(int id) {
+    public void delete(final int id) {
         log.info("delete user with id={}", id);
         repository.deleteExisted(id);
     }
