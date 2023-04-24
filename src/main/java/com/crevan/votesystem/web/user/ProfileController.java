9d7b4e26c484ef90fa5f0523c17e7916d627b5fb
Crevan <a.siverskii@gmail.com>
2023-04-24 21:25:33 +0300

    [update] ProfileController realisation

--- src/main/java/com/crevan/votesystem/web/user/ProfileController.java
+++ src/main/java/com/crevan/votesystem/web/user/ProfileController.java
@@ -1,14 +1,59 @@
 package com.crevan.votesystem.web.user;
 
+import com.crevan.votesystem.model.User;
+import com.crevan.votesystem.to.UserTo;
+import com.crevan.votesystem.util.UserUtil;
+import com.crevan.votesystem.util.ValidationUtil;
+import com.crevan.votesystem.web.AuthUser;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @Slf4j
 @RestController
+@AllArgsConstructor
 @RequestMapping(value = ProfileController.REST_URL, produces = MediaType.APPLICATION_JSON_VALUE)
 public class ProfileController extends AbstractUserController {
 
     static final String REST_URL = "/api/profile";
+
+    @GetMapping
+    public User get(@AuthenticationPrincipal final AuthUser authUser) {
+        log.info("get {}", authUser);
+        return authUser.getUser();
+    }
+
+    @DeleteMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@AuthenticationPrincipal final AuthUser authUser) {
+        log.info("delete {}", authUser);
+        super.delete(authUser.id());
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<User> register(@Valid @RequestBody final UserTo userTo) {
+        log.info("register {}", userTo);
+        ValidationUtil.checkNew(userTo);
+        User newUser = UserUtil.createNewFromTo(userTo);
+        URI uriOfNewResource = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path(REST_URL)
+                .build().toUri();
+        return ResponseEntity.created(uriOfNewResource).body(newUser);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void update(@RequestBody final UserTo userTo, @AuthenticationPrincipal final AuthUser authUser) {
+        log.info("update {} to {}", authUser, userTo);
+        repository.save(UserUtil.updateFromTo(authUser.getUser(), userTo));
+    }
 }
