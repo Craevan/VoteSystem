2f549f09643091be0ec76eaf563aa94f7fe31cb4
Crevan <a.siverskii@gmail.com>
2023-04-25 21:32:17 +0300

    [add] AbstractRestaurantController

---
+++ src/main/java/com/crevan/votesystem/web/restaurant/AbstractRestaurantController.java
@@ -0,0 +1,28 @@
+package com.crevan.votesystem.web.restaurant;
+
+import com.crevan.votesystem.model.Restaurant;
+import com.crevan.votesystem.repository.RestaurantRepository;
+import com.crevan.votesystem.util.ValidationUtil;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public abstract class AbstractRestaurantController {
+
+    protected RestaurantRepository repository;
+
+    public Restaurant create(final Restaurant restaurant) {
+        log.info("creating {}", restaurant);
+        return repository.save(restaurant);
+    }
+
+    public void update(final Restaurant restaurant, final int id) {
+        log.info("updating {} with id={}", restaurant, id);
+        ValidationUtil.assureIdConsistent(restaurant, id);
+        repository.save(restaurant);
+    }
+
+    public void delete(final int id) {
+        log.info("deleting restaurant with id={}", id);
+        repository.deleteExisted(id);
+    }
+}
