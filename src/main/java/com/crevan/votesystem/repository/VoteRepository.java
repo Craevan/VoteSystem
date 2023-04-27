0ae29d79d9be4536563d83c7bbfe7d5be0adbc2a
Crevan <a.siverskii@gmail.com>
2023-04-27 21:35:08 +0300

    [update] vote repository

--- src/main/java/com/crevan/votesystem/repository/VoteRepository.java
+++ src/main/java/com/crevan/votesystem/repository/VoteRepository.java
@@ -1,6 +1,13 @@
 package com.crevan.votesystem.repository;
 
 import com.crevan.votesystem.model.Vote;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
+@Transactional(readOnly = true)
 public interface VoteRepository extends BaseRepository<Vote> {
+
+    @Transactional
+    List<Vote> findAllByUserId(final int id);
 }
