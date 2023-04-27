d277289497868a9959d061496a9e904a95c4b8f8
Crevan <a.siverskii@gmail.com>
2023-04-27 21:34:36 +0300

    [update] vote model

--- src/main/java/com/crevan/votesystem/model/Vote.java
+++ src/main/java/com/crevan/votesystem/model/Vote.java
@@ -1,29 +1,37 @@
 package com.crevan.votesystem.model;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 import java.time.LocalDate;
 
 @Getter
 @Setter
 @Entity
-@ToString
 @NoArgsConstructor
 @Table(name = "vote")
+@ToString(callSuper = true, exclude = {"user", "restaurant"})
 public class Vote extends BaseEntity {
 
     @NotNull
     @ManyToOne(fetch = FetchType.LAZY)
+    @OnDelete(action = OnDeleteAction.CASCADE)
     @JoinColumn(name = "user_id", nullable = false)
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private User user;
 
     @NotNull
+    @JsonBackReference
     @ManyToOne(fetch = FetchType.LAZY)
+    @OnDelete(action = OnDeleteAction.CASCADE)
     @JoinColumn(name = "restaurant_id", nullable = false)
     private Restaurant restaurant;
 
