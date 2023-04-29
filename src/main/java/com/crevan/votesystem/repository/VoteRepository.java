e3a8cf9dc7ef7319cf2f439e69ec1b59915820d2
Crevan <a.siverskii@gmail.com>
2023-04-29 23:57:14 +0300

    [update] VoteRepository

--- src/main/java/com/crevan/votesystem/repository/VoteRepository.java
+++ src/main/java/com/crevan/votesystem/repository/VoteRepository.java
@@ -1,6 +1,8 @@
 package com.crevan.votesystem.repository;
 
 import com.crevan.votesystem.model.Vote;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -10,4 +12,9 @@ public interface VoteRepository extends BaseRepository<Vote> {
 
     @Transactional
     List<Vote> findAllByUserId(final int id);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE Vote v SET v.restaurant.id = ?2 WHERE v.user.id = ?1 AND v.voteDate = NOW()")
+    void update(final int userId, final int restaurantId);
 }
