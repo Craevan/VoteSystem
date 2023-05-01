d0bcf86183e70749157af848866e05ad37e7f90e
Crevan <a.siverskii@gmail.com>
2023-05-01 22:42:07 +0300

    [update] add constructor

--- src/main/java/com/crevan/votesystem/to/UserTo.java
+++ src/main/java/com/crevan/votesystem/to/UserTo.java
@@ -2,16 +2,14 @@ package com.crevan.votesystem.to;
 
 import com.crevan.votesystem.HasIdAndEmail;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 @Getter
 @Setter
-@AllArgsConstructor
+@NoArgsConstructor
 @ToString(callSuper = true)
 public class UserTo extends NamedEntityTo implements HasIdAndEmail {
 
@@ -20,6 +18,13 @@ public class UserTo extends NamedEntityTo implements HasIdAndEmail {
     @Size(max = 128)
     private String email;
 
+    @NotBlank
     @Size(max = 256)
     private String password;
+
+    public UserTo(Integer id, String name, String email, String password) {
+        super(id, name);
+        this.email = email;
+        this.password = password;
+    }
 }
