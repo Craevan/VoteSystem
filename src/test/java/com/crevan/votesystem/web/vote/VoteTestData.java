61123dd61697668c67ba46a33e2f99ea034c4401
Crevan <a.siverskii@gmail.com>
2023-05-10 20:26:07 +0300

    [update] test data for VoteController

--- src/test/java/com/crevan/votesystem/web/vote/VoteTestData.java
+++ src/test/java/com/crevan/votesystem/web/vote/VoteTestData.java
@@ -11,7 +11,7 @@ import static com.crevan.votesystem.web.user.UserTestData.user;
 public class VoteTestData {
     public static final Vote vote_1 = new Vote(1, user, kriek, LocalDate.of(2023, 5, 1));
     public static final Vote vote_2 = new Vote(2, user, altVelvet, LocalDate.of(2023, 5, 2));
-    public static final Vote vote_3 = new Vote(3, user, salhino, LocalDate.of(2023, 5, 3));
-    public static final VoteTo newTo = new VoteTo(4, 3, LocalDate.now());
+    public static final Vote vote_3 = new Vote(3, user, salhino, LocalDate.now());
+    public static final VoteTo newTo = new VoteTo(null, 3, null);
     public static final VoteTo invalidTo = new VoteTo();
 }
