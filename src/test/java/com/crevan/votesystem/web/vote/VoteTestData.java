214fb0f17d3b95a994fa9e754839a5bf6b46c9d8
Crevan <a.siverskii@gmail.com>
2023-05-11 13:36:19 +0300

    [refactor] move test data from tests

--- src/test/java/com/crevan/votesystem/web/vote/VoteTestData.java
+++ src/test/java/com/crevan/votesystem/web/vote/VoteTestData.java
@@ -4,6 +4,7 @@ import com.crevan.votesystem.model.Vote;
 import com.crevan.votesystem.to.VoteTo;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 
 import static com.crevan.votesystem.web.restaurant.RestaurantTestData.*;
 import static com.crevan.votesystem.web.user.UserTestData.user;
@@ -12,6 +13,10 @@ public class VoteTestData {
     public static final Vote vote_1 = new Vote(1, user, kriek, LocalDate.of(2023, 5, 1));
     public static final Vote vote_2 = new Vote(2, user, altVelvet, LocalDate.of(2023, 5, 2));
     public static final Vote vote_3 = new Vote(3, user, salhino, LocalDate.now());
-    public static final VoteTo newTo = new VoteTo(null, 3, null);
+    public static final VoteTo newTo = new VoteTo(null, PAULANER_ID, null);
     public static final VoteTo invalidTo = new VoteTo();
+    public static final VoteTo updatedTo = new VoteTo(null, PAULANER_ID, LocalDate.now());
+
+    public static final LocalTime afterTime = LocalTime.of(11, 0, 1);
+    public static final LocalTime beforeTime = LocalTime.of(10, 59, 59);
 }
