cfc3b067ae6d57993f115b128ef603729e04094e
a.siverskii <a.siverskii@gmail.com>
2023-09-05 07:06:35 +0300

    [fix] using primitive types for NotNull fields. [fix] create new invalid VoteTo for tests [add] constraint for voteDate

--- src/test/java/com/crevan/votesystem/web/vote/VoteTestData.java
+++ src/test/java/com/crevan/votesystem/web/vote/VoteTestData.java
@@ -14,7 +14,7 @@ public class VoteTestData {
     public static final Vote vote_2 = new Vote(2, user, altVelvet, LocalDate.of(2023, 5, 2));
     public static final Vote vote_3 = new Vote(3, user, salhino, LocalDate.now());
     public static final VoteTo newTo = new VoteTo(null, PAULANER_ID, null);
-    public static final VoteTo invalidTo = new VoteTo();
+    public static final VoteTo invalidTo = new VoteTo(null, 0, LocalDate.now().plusDays(1));
     public static final VoteTo updatedTo = new VoteTo(null, PAULANER_ID, LocalDate.now());
 
     public static final LocalTime afterTime = LocalTime.of(11, 0, 1);
